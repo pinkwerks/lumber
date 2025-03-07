f4aad314bf32678a2723bb5548719048956b8d40
Pink Bobsledder <pinkwerks@gmail.com>
2025-03-07 08:25:27 -0800

    Material animator

--- Packages/lumber/Runtime/Demo/WoodMaterialAnimator.cs
+++ Packages/lumber/Runtime/Demo/WoodMaterialAnimator.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class WoodMaterialAnimator : MonoBehaviour
 {
-    [SerializeField] private float _speed = 1;
+    [SerializeField] private Vector3 _originSpeed = Vector3.zero;
+    [SerializeField] private float _ringOffsetSpeed = 1;
     private Material _mat;
     private int _originHash = Shader.PropertyToID("_Origin");
+    private int _ringOffsetHash = Shader.PropertyToID("_RingOffset");
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +19,18 @@ public class WoodMaterialAnimator : MonoBehaviour
             Debug.LogError("Material does not have the property Origin");
             enabled = false;
         }
+        if (!_mat.HasProperty(_ringOffsetHash))
+        {
+            Debug.LogError("Material does not have the property RingOffset");
+            enabled = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        _mat.SetVector(_originHash, new Vector3(0, 0, Time.time * _speed));
+        var t = Time.time;
+        _mat.SetVector(_originHash, new Vector3(t * _originSpeed.x, t * _originSpeed.y, t * _originSpeed.z));
+        _mat.SetFloat(_ringOffsetHash, t * _ringOffsetSpeed);
     }
 }
